5c04b286f9ae308652879216c3d4a2fc2d0edc04
Antonio <antoniocarmagnani@hotmail.com>
2024-03-06 08:04:25 -0300

    add _move effect and box_shadow

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -15,29 +15,3 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('Lang').innerHTML = data;
         });
 });
-
-
-// PAGES
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('html/pages/projects.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('Projects').innerHTML = data;
-        });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('html/pages/about.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('About').innerHTML = data;
-        });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('html/pages/contact.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('Contact').innerHTML = data;
-        });
-});
